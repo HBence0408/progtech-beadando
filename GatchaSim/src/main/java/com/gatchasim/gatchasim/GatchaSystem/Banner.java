1d62a50f7c51be7624c5b3e7584bff126f3a0d50
HBence0408 <61012730+HBence0408@users.noreply.github.com>
2025-04-19 18:15:33 +0200

    Gatcha system
    gatcha system singelton pull funkció kész, van egy banner absztrakt osztály amiből a többi banner lesz valamint , gatcha item amiből a 3star 4star 5star osztályok vannak.

---
+++ GatchaSim/src/main/java/com/gatchasim/gatchasim/GatchaSystem/Banner.java
@@ -0,0 +1,33 @@
+package com.gatchasim.gatchasim.GatchaSystem;
+
+import java.util.List;
+
+public abstract class Banner {
+
+    public abstract int GetPity(); // itt az osztály static fieldjét kell vissza adni majd
+
+    public abstract int GetPullsSinceLast4Star();
+    public abstract int GetPullsSinceLast5Star();
+
+    public  abstract boolean IsOn5050(); // elvesztette e az utóbbbi fifty fiftyt
+
+    protected List<ThreeStarItem> threeStarItems; // itt majd adatbázisbó lekérés kell
+    protected List<FourStarItem> fourStarItems; // itt majd adatbázisbó lekérés kell
+    protected List<FiveStarItem> fiveStarItems; // itt majd adatbázisbó lekérés kell
+
+    public ThreeStarItem ThreeStarPull(){
+        return threeStarItems.getFirst();        // not final
+    }
+
+    public FourStarItem FourStarPull(){
+        return fourStarItems.getFirst();              // not final
+    }
+
+    public FiveStarItem FiveStarPull(){
+        return fiveStarItems.getFirst();            // not final
+    }
+
+}
+
+// ebből származtatni a weaponm, main és base bannert mindegyikben pedig static field számol Pityt,
+// így a main bannerek egymással megosztják, a weapon is a csak a weaponnal
