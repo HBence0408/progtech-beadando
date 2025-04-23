590e39ff7e113b494713804ffcce2609b2c43e75
HBence0408 <61012730+HBence0408@users.noreply.github.com>
2025-04-23 12:51:55 +0200

    command invoker
    csináltam egy command invoker osztályt, ami kkésőbb bővíthető hogy naplózzon, undo, vagy hogy másik szálon indítson commandot, de egyenlőre csak simán futtatja a commandot

--- GatchaSim/src/main/java/com/gatchasim/gatchasim/Database/Database.java
+++ GatchaSim/src/main/java/com/gatchasim/gatchasim/Database/Database.java
@@ -13,3 +13,41 @@ public abstract class Database {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
+/*
+*
+*
+* egy példa a használatára a command-nak
+*
+* public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        TextField usernameField = new TextField();
+        TextField emailField = new TextField();
+        Button addButton = new Button("Add User");
+
+        UserDatabase database = new UserDatabase();
+        CommandInvoker invoker = new CommandInvoker(); nekünk még nincs ilyen invoker nem tudom hogy szükséges lenne e
+        * lehet közvetlenül is hívni, de azért létre hoztam egy command invokert,
+
+        addButton.setOnAction(e -> {
+            String username = usernameField.getText();
+            String email = emailField.getText();
+            Command command = new AddUserCommand(database, username, email);
+            invoker.execute(command);
+        });
+
+        VBox layout = new VBox(10, usernameField, emailField, addButton);
+        Scene scene = new Scene(layout, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("User Manager");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+*
+* */
