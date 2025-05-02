ca0ac5c1598d87d2226f718d1ddbc7b6d5554394
HBence0408 <61012730+HBence0408@users.noreply.github.com>
2025-05-02 18:35:40 +0200

    Banner class
    a banner osztály példányosítható és le lehet kérni az adott bannerről random 3star, 4star vagy 5star itemet az adatbázisból (ha meg van hozzá a kapcsolat és tábla is persze)

--- GatchaSim/src/main/java/com/gatchasim/gatchasim/GatchaSystem/Banner.java
+++ GatchaSim/src/main/java/com/gatchasim/gatchasim/GatchaSystem/Banner.java
@@ -1,39 +1,70 @@
 package com.gatchasim.gatchasim.GatchaSystem;
 
-import com.gatchasim.gatchasim.Database.ICommand;
+import com.gatchasim.gatchasim.Database.Banner.GetItemsCommand;
 
 import java.util.List;
+import java.util.Random;
 
-public abstract class Banner {
+public class Banner {
 
-    public abstract int GetPity(); // itt az osztály static fieldjét kell vissza adni majd
+    Random rnd = new Random();
 
-    public abstract int GetPullsSinceLast4Star();
-    public abstract int GetPullsSinceLast5Star();
+    private String BannerName;
 
-    public  abstract boolean IsOn5050(); // elvesztette e az utóbbbi fifty fiftyt
+    private int pity;
+    public int GetPity()
+    {
+        return pity;
+    }
 
-    protected ICommand get3starCommand;
-    protected ICommand get4starCommand;
-    protected ICommand get5starCommand;
+    private int pullsSinceLast4Star;
+    public int GetPullsSinceLast4Star(){
+        return pullsSinceLast4Star;
+    }
 
-    protected List<ThreeStarItem> threeStarItems; // itt majd adatbázisbó lekérés kell
-    protected List<FourStarItem> fourStarItems; // itt majd adatbázisbó lekérés kell
-    protected List<FiveStarItem> fiveStarItems; // itt majd adatbázisbó lekérés kell
+    private int pullsSinceLast5Star;
+    public int GetPullsSinceLast5Star(){
+        return pullsSinceLast5Star;
+    }
+
+    private boolean isOn5050;
+
+    public boolean IsOn5050(){
+        return isOn5050;
+    } // elvesztette e az utóbbbi fifty fiftyt
+
+    protected GetItemsCommand get3starCommand;
+    protected GetItemsCommand get4starCommand;
+    protected GetItemsCommand get5starCommand;
+
+    public Banner(String name){
+        BannerName = name;
+
+        get3starCommand = new GetItemsCommand("3star", BannerName);
+        get4starCommand = new GetItemsCommand("4star", BannerName);
+        get5starCommand = new GetItemsCommand("5star", BannerName);
+    }
 
     public ThreeStarItem ThreeStarPull(){
-        return threeStarItems.getFirst();        // not final
+        List<GatchaItem> items = get3starCommand.execute();
+        GatchaItem item = items.get(rnd.nextInt(items.size()));
+        item = new ThreeStarItem(item);
+        return (ThreeStarItem) item;
     }
 
     public FourStarItem FourStarPull(){
-        return fourStarItems.getFirst();              // not final
+        List<GatchaItem> items = get4starCommand.execute();
+        GatchaItem item = items.get(rnd.nextInt(items.size()));
+        item = new FourStarItem(item);
+        return (FourStarItem) item;
     }
 
     public FiveStarItem FiveStarPull(){
-        return fiveStarItems.getFirst();            // not final
+        List<GatchaItem> items = get5starCommand.execute();
+        GatchaItem item = items.get(rnd.nextInt(items.size()));
+        item = new FiveStarItem(item);
+        return (FiveStarItem) item;
+        // TODO: 50 50
     }
 
 }
-
-// ebből származtatni a weaponm, main és base bannert mindegyikben pedig static field számol Pityt,
-// így a main bannerek egymással megosztják, a weapon is a csak a weaponnal
